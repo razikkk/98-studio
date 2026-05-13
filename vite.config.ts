fa2d15a8b354dc7521ec84b6b1e2e5ee2f4b34f5
Razik <razik3407@gmail.com>
2026-05-13 19:56:39 +0530

    18 commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import { nitro } from 'nitro/vite' // Add this
+import { nitro } from 'nitro/vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from "path";
@@ -8,19 +8,13 @@ import path from "path";
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    nitro({
-      preset: 'vercel' // Explicitly tells Nitro to build for Vercel
-    }), // No deployment property here anymore
-    tailwindcss(),
+    nitro(),
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 3000,
-    strictPort: true, 
   },
 });

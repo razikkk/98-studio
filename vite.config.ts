3666eee21d38472918845a7d29112c9f47c826a9
Razik <razik3407@gmail.com>
2026-05-13 18:48:34 +0530

    16 commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from "path"; // Import path to handle the directory link
+import path from "path";
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
+    tanstackStart(), // No deployment property here anymore
     tailwindcss(),
     react(),
   ],
   resolve: {
     alias: {
-      // This tells Vite that @/ means the src/ folder
       "@": path.resolve(__dirname, "./src"),
     },
   },

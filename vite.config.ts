ecffd03207d3c23f6e2321bfbbdc578e49d2e2e3
Razik <razik3407@gmail.com>
2026-05-13 18:28:23 +0530

    12 commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react'; // 1. Add this import
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
-    react(), // 2. Add this to the plugins array
+    react(),
   ],
-  // Keep this for Vercel
-  server: {
-    preset: 'vercel',
-  },
 });
